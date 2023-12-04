9e6eefe1a632b129885f5c661e395c7cfc23eb7a
RaphaDiniz <dinizraphael45@gmail.com>
2023-12-04 19:02:51 -0300

    laser and cmd

--- src/cmd_vel_subscriber.py
+++ src/cmd_vel_subscriber.py
@@ -4,44 +4,42 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 import serial
-#Raphael Diniz relampago_marquinhos_robot
 
 class RobotController:
-    def __init__(self, serial_port):
-        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
-        self.serial_port = serial.Serial(serial_port, 115200, timeout=1)
+
+    def __init__(self):
+
+        self.sub_cmd_vel = rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
+        self.serial_port_name = rospy.get_param('~serial_port', '/dev/ttyUSB1')
+        self.serial_port = serial.Serial(self.serial_port_name, 115200, timeout=1)
         self.twist = Twist()
-        self.default_command = 'S'
-        self.obstacle_command = 'D'
-        self.obstacle_distance = 0.03  # Distância mínima para detecção de obstáculos
-        self.obstacle_detected = False
 
-    def scan_callback(self, scan_data):
-        # Imprimir os valores das leituras do LIDAR
-        # rospy.loginfo("Leituras do LIDAR: {}".format(scan_data.ranges))
-        print(scan_data.ranges)
-        # Verificar se há obstáculo à frente e ajustar o movimento
-        if min(scan_data.ranges[1:31]) < self.obstacle_distance:
-            if not self.obstacle_detected:
-                # Se um obstáculo foi detectado, envie o comando 'D' e marque o obstáculo como detectado
-                self.send_serial_command(self.obstacle_command)
-                rospy.loginfo("Obstáculo detectado. Comando enviado para a porta serial: {}".format(self.obstacle_command))
-                self.obstacle_detected = True
+    def cancel(self):
+        self.sub_cmd_vel.unregister()
+        rospy.loginfo("Shutting down this node.")
+
+    def cmd_vel_callback(self, twist_cmd):
+        # Callback para receber comandos do tópico cmd_vel
+        linear = twist_cmd.linear.x
+        angular = twist_cmd.angular.z
+
+        # Lógica para converter os comandos Twist em comandos específicos do seu robô
+        if linear > 0:
+            command = 'S' 
+
+        elif linear < 0:
+            command = 'W'
+
+        elif angular > 0:
+            command = 'A' 
+
+        elif angular < 0: 
+            command = 'D'
+
         else:
-            # Se não há obstáculo, envie o comando 'S' e marque o obstáculo como não detectado
-            self.send_serial_command(self.default_command)
-            rospy.loginfo("Nenhum obstáculo detectado. Comando enviado para a porta serial: {}".format(self.default_command))
-            self.obstacle_detected = False
+            command = 'K'  # Parar o robô
 
-    def control_loop(self):
-        rospy.loginfo("Entrando no loop de controle.")
-        rate = rospy.Rate(10)  # 10 Hz
-        while not rospy.is_shutdown():
-            # Publica os comandos Twist para movimentar o robô
-            self.cmd_pub.publish(self.twist)
-            rate.sleep()
-        rospy.loginfo("Saindo do loop de controle.")
+        self.send_serial_command(command)
 
     def send_serial_command(self, command):
         # Envia o comando para a porta serial
@@ -52,11 +50,11 @@ class RobotController:
             rospy.logerr("Erro ao escrever na porta serial: {}".format(e))
 
 if __name__ == '__main__':
-    rospy.init_node('robot_controller')
-    serial_port = '/dev/ttyUSB0'  # Substitua pela sua porta serial correta
-    controller = RobotController(serial_port)
-    try:
-        controller.control_loop()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('robot_controller', anonymous=False)
+    controller = RobotController()
+
+    # Subscreva o tópico cmd_vel com a função cmd_vel_callback
+    rospy.Subscriber('/cmd_vel', Twist, controller.cmd_vel_callback)
+
     rospy.spin()
+    controller.cancel()
--- src/laser_Avoidance.py
+++ src/laser_Avoidance.py
@@ -32,7 +32,6 @@ class laserAvoid:
         self.front_warning = 0
         self.before_command = ''
         self.sub_laser = rospy.Subscriber('/scan', LaserScan, self.registerScan)
-        self.sub_cmd_vel = rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
         self.serial_port = rospy.get_param('~serial_port', '/dev/ttyUSB1')
         self.serial_port = serial.Serial(self.serial_port, 115200, timeout=1)
 
@@ -71,69 +70,45 @@ class laserAvoid:
                     if ranges[i] < self.ResponseDist: self.front_warning += 1
         # print (self.Left_warning,self.front_warning,self.Right_warning)
 
-    def cmd_vel_callback(self, twist_cmd):
-        # Callback para receber comandos do tópico cmd_vel
-        linear = twist_cmd.linear.x
-        angular = twist_cmd.angular.z
-
-        if (linear > 0 and angular < 0): #LETRA U
-            self.autonomous_mode = True
-
-        # Lógica para converter os comandos Twist em comandos específicos do seu robô
-        if linear > 0:
-            command = 'S' 
-        elif linear < 0:
-            command = 'W'
-        elif angular > 0:
-            command = 'A' 
-        elif angular < 0: 
-            command = 'D'
-        else:
-            command = 'K'  # Parar o robô
-
-        self.send_serial_command(command)
-
     def robot_move(self):
-        linear = self.twist_cmd.linear.x
-        angular = self.twist_cmd.angular.z
-
-        if self.autonomous_mode:
-
-            if (linear > 0 and angular < 0): #LETRA U
-                self.autonomous_mode = False
-
+        while not rospy.is_shutdown():
             if self.switch:
                 if self.Moving:
                     self.send_serial_command(self.default_command)
                     self.Moving = not self.Moving
-                return
-
+                continue
             self.Moving = True
-
             # Lógica para controle autônomo
             if self.front_warning > 10 and self.Left_warning > 10 and self.Right_warning > 10:
                 self.send_serial_command(self.obstacle_all_command)
+                
             elif self.front_warning > 10 and self.Left_warning <= 10 and self.Right_warning > 10:
                 self.send_serial_command(self.obstacle_left_command)
+                
                 if self.Left_warning > 10 and self.Right_warning <= 10:
                     self.send_serial_command(self.obstacle_command)
+
             elif self.front_warning > 10 and self.Left_warning > 10 and self.Right_warning <= 10:
                 self.send_serial_command(self.obstacle_command)
+                
                 if self.Left_warning <= 10 and self.Right_warning > 10:
                     self.send_serial_command(self.obstacle_left_command)
+
             elif self.front_warning > 10 and self.Left_warning < 10 and self.Right_warning < 10:
                 self.send_serial_command(self.obstacle_left_command)
+                
             elif self.front_warning < 10 and self.Left_warning > 10 and self.Right_warning > 10:
                 self.send_serial_command(self.obstacle_command)
+      
             elif self.front_warning < 10 and self.Left_warning > 10 and self.Right_warning <= 10:
                 self.send_serial_command(self.obstacle_left_command)
+                
             elif self.front_warning < 10 and self.Left_warning <= 10 and self.Right_warning > 10:
                 self.send_serial_command(self.obstacle_command)
+                
             elif self.front_warning <= 10 and (self.Left_warning <= 10 and self.Right_warning <= 10):
                 self.send_serial_command(self.default_command)
-
         self.r.sleep()
-
     
     def send_serial_command(self, command):
         # Envia o comando para a porta serial
@@ -147,23 +122,10 @@ class laserAvoid:
         except serial.SerialException as e:
             rospy.logerr("Erro ao escrever na porta serial: {}".format(e))
 
-    def switch_control(self, twist_cmd):
-        linear = twist_cmd.linear.x
-        angular = twist_cmd.angular.z
-        
-        if self.autonomous_mode:
-            self.robot_move()
-        if not self.autonomous_mode:
-            self.cmd_vel_callback(twist_cmd)
-
-
 
 if __name__ == '__main__':
     rospy.init_node('robot_controller', anonymous=False)
     tracker = laserAvoid()
-
-    # Subscreva o tópico cmd_vel com a função switch_control
-    rospy.Subscriber('/cmd_vel', Twist, tracker.switch_control)
-
+    tracker.robot_move()
     rospy.spin()
     tracker.cancel()
